541bca8ebcc22918e3d9a77009aeb0488c958dcd
RaghavReddy <raghavendra.reddy@kaplan.com>
2013-11-04 22:46:02 -0500

    Login module change

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,39 @@
+utils.loadTemplate(['HomeView', 'HeaderView', 'VideoView', 'VideoListItemView', 'VideoHotspotView', 'AboutView'], function() {
+    app = new AppRouter();
+    Backbone.history.start();
+});
+
+var BaseRouter = Backbone.Router.extend({
+      before: function(){},
+      after: function(){},
+      route : function(route, name, callback){
+        debugger;
+          if (!_.isRegExp(route)) route = this._routeToRegExp(route);
+          if (_.isFunction(name)) {
+              callback = name;
+              name = '';
+          }
+          if (!callback) callback = this[name];
+
+          var router = this;
+
+          Backbone.history.route(route, function(fragment) {
+              var args = router._extractParameters(route, fragment);
+
+              var next = function(){
+                  callback && callback.apply(router, args);
+                  router.trigger.apply(router, ['route:' + name].concat(args));
+                  router.trigger('route', name, args);
+                  Backbone.history.trigger('route', router, name, args);
+                  router.after.apply(router, args);
+              }
+              router.before.apply(router, [args, next]);
+          });
+          return this;
+      }
+  });
+
+
 var AppRouter = Backbone.Router.extend({
 
     routes: {
@@ -6,8 +42,57 @@ var AppRouter = Backbone.Router.extend({
         "videos/page/:page" : "list",
         "videos/add"        : "addVideo",
         "videos/:id"        : "videoDetails",
-        "about"             : "about"
+        "about"             : "about",
+        'login'             : 'showLogin'
     },
+
+    // Routes that need authentication and if user is not authenticated
+      // gets redirect to login page
+      requresAuth : ['#profile'],
+
+      // Routes that should not be accessible if user is authenticated
+      // for example, login, register, forgetpasword ...
+      preventAccessWhenAuth : ['#login'],
+
+      before : function(params, next){
+          //Checking if user is authenticated or not
+          //then check the path if the path requires authentication
+          var isAuth = Session.get('authenticated');
+          var path = Backbone.history.location.hash;
+          var needAuth = _.contains(this.requresAuth, path);
+          var cancleAccess = _.contains(this.preventAccessWhenAuth, path);
+
+          if(needAuth && !isAuth){
+              //If user gets redirect to login because wanted to access
+              // to a route that requires login, save the path in session
+              // to redirect the user back to path after successful login
+              Session.set('redirectFrom', path);
+              Backbone.history.navigate('login', { trigger : true });
+          }else if(isAuth && cancleAccess){
+              //User is authenticated and tries to go to login, register ...
+              // so redirect the user to home page
+              Backbone.history.navigate('', { trigger : true });
+          }else{
+              //No problem, handle the route!!
+              return next();
+          }
+      },
+
+    fetchError : function(error){
+          //If during fetching data from server, session expired
+          // and server send 401, call getAuth to get the new CSRF
+          // and reset the session settings and then redirect the user
+          // to login
+          if(error.status === 401){
+              Session.getAuth(function(){
+                  Backbone.history.navigate('login', { trigger : true });
+              });
+          }
+      },
+
+      after : function(){
+          //empty
+      },
 
     initialize: function () {
         this.headerView = new HeaderView();
@@ -61,3 +146,4 @@ utils.loadTemplate(['HomeView', 'HeaderView', 'VideoView', 'VideoListItemView', 
     app = new AppRouter();
     Backbone.history.start();
 });
+
